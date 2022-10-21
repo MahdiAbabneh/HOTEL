12e9da6eb9dc2f4abae8ef311760e38fc9389f70
Mahdi <mahdiababneh37@gmail.com>
2022-10-21 19:35:07 +0300

    commit Hotel Project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.hotel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.sql;
+    requires rt;
+
+    opens com.example.hotel to javafx.fxml;
+    exports com.example.hotel;
+}
